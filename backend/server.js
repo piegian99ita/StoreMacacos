62af57995fe0f2aea66a0f4c65b8877a85fc82ff
piegian99ita <pietro.giannini@studenti.unitn.it>
2024-09-25 16:27:24 +0200

    v1.0.1

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const app = require('./app');
 const path = require('path');
 const port =  3050;
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 mongoose.connect(process.env.DB_URL).then(
         app.listen(port, () => {
