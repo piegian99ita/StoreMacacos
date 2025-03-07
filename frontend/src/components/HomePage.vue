b072e3ab18d7d62d55e2db3ea180c49e4f3a2806
piegian99ita <pietro.giannini@studenti.unitn.it>
2025-03-07 17:12:10 +0100

    aggiustato frontend

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -273,7 +273,7 @@ export default {
   display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(hsl(251, 57%, 22%)50%, hsl(250, 51%, 11%) 90%);
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
   
@@ -283,7 +283,7 @@ export default {
   display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(#241858 50%, #130E2A);
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
   
@@ -292,7 +292,7 @@ export default {
   display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(hsl(251, 57%, 22%)50%, hsl(250, 51%, 11%) 90%);
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
   
@@ -600,7 +600,7 @@ export default {
   display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(hsl(251, 57%, 22%), hsl(250, 51%, 11%) );
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
   
@@ -610,7 +610,7 @@ export default {
     display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(hsl(251, 57%, 22%) 50%, hsl(250, 51%, 11%) 90%);
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
 
@@ -619,7 +619,7 @@ export default {
   display:flex;
   flex-direction: column;
   align-items: center;
-  background:radial-gradient(hsl(251, 57%, 22%) 50%, hsl(250, 51%, 11%) 90%);
+  background:radial-gradient(#241858 50%, #130E2A 90%);
   border-radius: 5%;
   filter: drop-shadow(0.2px 0.2px 5px rgb(255, 255, 255));
 }
